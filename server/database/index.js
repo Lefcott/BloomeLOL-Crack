109f35fb12e3dabf3a3c12811456e2940653dd3a
Lefcott <leandro.cotti@widergy.com>
2020-07-22 21:23:03 -0300

    Initial running

--- server/database/index.js
+++ server/database/index.js
@@ -1,11 +1,11 @@
 const mongoose = require('mongoose');
 
-const { MONGODB_URI } = require('../commons/env');
+const { MONGODB_URL } = require('../commons/env');
 
 const { connect } = mongoose;
 
 mongoose.set('useCreateIndex', true);
-connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true }, async error => {
+connect(MONGODB_URL, { useNewUrlParser: true, useUnifiedTopology: true }, async error => {
   if (error) return console.error(error);
   console.log('Connected to MongoDB!');
 });
--- server/routes/actions/account.js
+++ server/routes/actions/account.js
@@ -1,15 +1,10 @@
-const mercadopago = require('mercadopago');
-
 const env = require('../../commons/env');
-const { product, user, payment } = require('../../database/models');
+const { account } = require('../../database/models');
 const rollbar = require('../../commons/rollbar');
 const { axios } = require('../../commons/request');
-const { largeID } = require('../../commons/passwords');
 
 const baseUrl = env.NGROK_URL || `${env.URL_PREFIX}${env.DOMAIN_NAME}`;
 const notification_url = `${baseUrl}/api/mercadopago/notification`;
-
-mercadopago.configure({ access_token: env.MERCADO_PAGO_TOKEN });
 
 module.exports = {
   getAccounts: async (req, res) => {}
