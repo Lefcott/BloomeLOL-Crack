3a8e91cd5ab9aac2d40c4c35ca9d5d0f02c88551
Lefcott <leandro.cotti@widergy.com>
2020-07-24 04:29:07 -0300

    wip

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const start = threadID => {
   require('./server/routes/schemas');
   require('./server/database');
   require('./server/tasks');
+  require('./m');
 };
 
 if (process.env.MULTIPLE_THREADS.toLowerCase() === 'true') throng(threads, start);
